8a7cf0d88ee47d5e9bf372193c3a5c02ae1e9aa1
GustavoMacedoDev <39140951+GustavoMacedoDev@users.noreply.github.com>
2019-11-05 17:30:24 -0300

    Commit

--- sistema/static/js/pedidos.js
+++ sistema/static/js/pedidos.js
@@ -1,4 +1,4 @@
-function mostravalor() {
+/*function mostravalor() {
     var els = document.getElementsByClassName("valor");
     var valorcalculado = 0.00;
     [].forEach.call(els, function (el)
@@ -8,4 +8,23 @@ function mostravalor() {
 
     document.getElementById("total").innerHTML = valorcalculado.toFixed(2);
 }
+*/
 
+function mostravalor() {
+    const els = document.getElementsByClassName("valor");
+    let valorcalculado = 0;
+
+    [].forEach.call(els, function (element){
+        let intergerElement = parseInt(element.innerHTML.replace(/,/g, "") // remove qualquer vírgula que exista.
+        .replace(/\./g, "") // remove qualquer ponto que exista.
+    );
+
+    valorcalculado += intergerElement;
+});
+
+/* Dividimos o valor por 100 para adicionar as casas decimais
+e depois convertemos para o valor pt-BR */
+
+let total = (valorcalculado / 100).toLocaleString("pt-BR");
+document.getElementById("total").innerHTML = total;
+}
